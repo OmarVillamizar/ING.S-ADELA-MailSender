3b5cb36e403e455f4b0cb89ecdc85aff03020fe1
SGuerrero <juansebastiangb@ufps.edu.co>
2025-11-05 23:06:45 -0500

    Inicializacion del proyecto con springboot y las dependecias necesarias

---
+++ src/main/java/com/adela/mailsender/demo/AdelaMailSenderApplication.java
@@ -0,0 +1,13 @@
+package com.adela.mailsender.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdelaMailSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdelaMailSenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adela/mailsender/demo/AdelaMailSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adela.mailsender.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdelaMailSenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
